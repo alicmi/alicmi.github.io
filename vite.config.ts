16a5b70864e84ebda6e55708d0951c35b0308517
alicmi <alina.schmeiss@web.de>
2026-03-08 12:41:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets are loaded relatively (crucial for GitHub Pages)
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
